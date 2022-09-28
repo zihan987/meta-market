010e2be0cc8c6a351aa063b0acdee2ac23fc8d19
Tianyu Zhu <41713096+zihan987@users.noreply.github.com>
2022-09-28 20:09:44 +0800

    upload demo

---
+++ assets/type/index.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/type",["./type.ts"],(function(){"use strict";return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/type.ts",["cc"],(function(t){"use strict";var c;return{setters:[function(t){c=t.cclegacy}],execute:function(){c._RF.push({},"7fc0f8mIONKOqG5/Jtd6O2C","type",void 0);t("conf",{}),t("xxx",(function(){return Math.floor(255*Math.random()+1)}));c._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/type', 'chunks:///_virtual/type'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
